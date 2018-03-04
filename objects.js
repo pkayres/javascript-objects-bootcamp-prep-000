6da3034dfa52839739a672ffd8bc0e37fa0a91b6
Pete A <peter.ayresmusic@gmail.com>
2018-03-04 22:07:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   Coldplay:'sparks'
 }
 function updatePlaylist(playlist, artistName, songTitle){
-   Object.assign({}, playlist, { The Beatles: ['yesterday'] })
+   Object.assign({ The Beatles: ['yesterday'] })
 }
 function removeFromPlaylist(playlist,artistName){
   delete playlist.artistName; 
