8a2d20365625ae193f15e97c2748513857287afe
Pete A <peter.ayresmusic@gmail.com>
2018-03-04 21:56:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   Coldplay:'sparks'
 }
 function updatePlaylist(playlist, artistName, songTitle){
-   
+   playlist.Beatles = 'yogurt';
   return playlist 
 }
 function removeFromPlaylist(playlist,artistName){
