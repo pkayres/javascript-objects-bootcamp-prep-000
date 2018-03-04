c5ba1f58d57daf3914e2dcac4d7a00173fa5910f
Pete A <peter.ayresmusic@gmail.com>
2018-03-04 21:03:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {
   Coldplay:'sparks'
 }
 function updatePlaylist(playlist,artistName, songTitle){ 
-  return Object.assign({},playlist,'Phil Ochs', 'Heres to the State of Mississippi')
+  return Object.assign({playlist,artistName, songTitle})
 }
